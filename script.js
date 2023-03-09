437466b4e227b3d4a2f0d3de2b871c74dcec401c
MiraiDXD <farisryze@gmail.com>
2023-03-10 02:27:51 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+
+
+let cartBox = document.getElementById('cartBox')
+cartBox.style.display = 'none'
+function Button () {
+    cartBox.style.display = 'flex'
+}
+
+function Close () {
+    cartBox.style.display = 'none'
+}
